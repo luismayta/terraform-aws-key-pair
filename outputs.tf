84a9121a35193801c9cde6cfdf2653962ff7792b
luismayta <luis@hadenlabs.com>
2021-10-11 16:37:43 -0500

    build: generate output name (#8)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance" {
   description = "key_pair of instance."
   value       = aws_key_pair.this
 }
+
+output "name" {
+  description = "name of key_pair."
+  value       = aws_key_pair.this.key_name
+}
