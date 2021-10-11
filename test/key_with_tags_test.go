c690550313d7161c8d7bc46a1de5a6cae71105e2
luismayta <luis@hadenlabs.com>
2021-10-11 15:57:06 -0500

    test: implement key with tags (#5)

---
+++ test/key_with_tags_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/hadenlabs/terraform-aws-key-pair/config"
+	"github.com/hadenlabs/terraform-aws-key-pair/internal/app/external/faker"
+	"github.com/hadenlabs/terraform-aws-key-pair/internal/common/log"
+)
+
+func TestWithTagsSuccess(t *testing.T) {
+	t.Parallel()
+	conf := config.Must()
+	name := faker.Key().Name()
+	tags := map[string]interface{}{
+		"key":  "value",
+		"key2": "value",
+		"key3": "value",
+		"key4": "value",
+	}
+	publicKey := "../fixtures/keys/testing.pub"
+	logger := log.Factory(*conf)
+	logger.Debugf(
+		"values for test terraform-aws-key-pair is",
+		"name", name,
+	)
+
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "key-with-tags",
+		Upgrade:      true,
+		Vars: map[string]interface{}{
+			"name":       name,
+			"public_key": publicKey,
+			"tags":       tags,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+	outputInstance := terraform.Output(t, terraformOptions, "instance")
+	assert.NotEmpty(t, outputInstance, outputInstance)
+}
